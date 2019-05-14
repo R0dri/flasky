2aaf2eff092881c6b573b928c6c609781f5d66bc
R0dri <rodri.mendoza.t@gmail.com>
2019-05-14 11:07:55 -0400

    Added restful api

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 from flask_security.forms import RegisterForm, StringField, Required, LoginForm
+from flask_restful import Resource, Api
+
+import json
+from collections import namedtuple
+
+
+from flask_debugtoolbar import DebugToolbarExtension
 
 # Create app
 app = Flask(__name__)
@@ -12,7 +19,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:B1Admin@@MYMSSQL'
 app.config['SECURITY_REGISTERABLE'] = True
 app.config['SECURITY_PASSWORD_SALT'] = 'somesupersecretstring'
 app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
-app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = 'username'
+app.config['DEBUG_TB_ENABLED'] = True
+
+api = Api(app)
+toolbar=DebugToolbarExtension(app)
 
 # Create database connection object
 db = SQLAlchemy(app)
@@ -27,23 +37,54 @@ class Role(db.Model, RoleMixin):
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
+class OSCL(db.Model):
+    # __tablename__ = 'OSCL'
+    id =db.Column(db.Integer(), primary_key=True)
+    priority = db.Column(db.Integer())
+    subject = db.Column(db.String(100))
+    problemTyp = db.Column(db.String(100))
+    ProSubType = db.Column(db.String(100))
+    callType = db.Column(db.String(100))
+    contactCode = db.Column(db.String(100))
+    BPContact = db.Column(db.String(100))
+    createTime = db.Column(db.DateTime())
+    BPPhone1 = db.Column(db.String(20))
+    BPCellular = db.Column(db.String(20))
+    BPE_Mail = db.Column(db.String(50))
+    # empresa = db.Column(db.String(80), unique=True)
+    BPProjCode = db.Column(db.String(80))
+    description = db.Column(db.String(8000))
+    # trgtPath = db.Column()
+
 class User(db.Model, UserMixin):
+    # __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
     username = db.Column(db.String(255), unique=True)
-    password = db.Column(db.String(255))
+    telefono = db.Column(db.String(255))
+    celular = db.Column(db.String(255))
     first_name = db.Column(db.String(255))
     last_name = db.Column(db.String(255))
+    empresa = db.Column(db.String(255))
+    password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role', secondary=roles_users,
-                            backref=db.backref('users', lazy='dynamic'))
+    roles = db.relationship('Role', secondary='roles_users', backref=db.backref('users', lazy='dynamic'))
+
+
 
 class ExtendedLoginForm(LoginForm):
-    email = StringField('username', [Required()])
+    email = StringField('Usuario o Mail', [Required()])
+
 
 class ExtendedRegisterForm(RegisterForm):
-    email = StringField('')
-    username = StringField('username', [Required()])
+    username = StringField('Nombre de Usuario', [Required()])
+    email = StringField('Mail', [Required()])
+    telefono = StringField('Telefono', [Required()])
+    celular = StringField('Celular', [Required()])
+    first_name = StringField('Primer Nombre', [Required()])
+    last_name = StringField('Apellido', [Required()])
+    empresa = StringField('Nombre Empersa', [Required()])
 
 # Setup Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
@@ -59,10 +100,10 @@ def context_processor():
 # @app.before_first_request
 # def create_user():
 #     db.create_all()
-#     user_datastore.create_user(username='r', password='p')
+#     user_datastore.create_user(email='r', password='p')
 #     db.session.commit()
 
-# Views
+# # Views
 @app.route('/')
 # @login_required
 def home():
@@ -74,10 +115,79 @@ def test():
 
 @app.route('/<username>')
 @login_required
-def user(username):
+def euser(username):
     user = User.query.filter_by(username=username).first();
     return render_template('user.html', user=user)
 
+class llamadaServicio(Resource):
+    def get(self):
+        # return render_template('test.html')
+        ids = "asdf"
+        se = db.text("SELECT * FROM [user] WHERE username = :ids")
+        que = db.engine.execute(se, ids = ids)
+        un = [dict(row) for row in que]
+        un = un[0]
+        return jsonify(un["email"])
+
+    def post(self):
+        sn = request.get_json()
+        # u = db.engine.execute("SELECT * FROM user")
+        # su = jsonify([dict(row) for row in u])
+
+        se = db.text("SELECT * FROM [user] WHERE username = :ids")
+        # u = db.engine.execute(se, ids = sn["BPContact"]).fetchall()
+        ids = "asdf"
+        u = db.engine.execute(se, ids=ids).fetchall()
+        su = [dict(row) for row in u]
+        su = su[0]
+        sas = OSCL(priority=sn["priority"], subject=sn["subject"], problemTyp=sn["problemTyp"], ProSubType=sn["ProSubType"], callType=sn["callType"], contactCode=su["id"], BPContact=su["username"], createTime=su["confirmed_at"], BPPhone1=su["telefono"], BPCellular=su["celular"], BPE_Mail=su["email"], BPProjCode=su["empresa"], description=sn["description"])
+
+
+        # sas = OSCL(priority=sn["priority"], subject=sn["subject"], problemTyp=sn["problemTyp"], ProSubType=sn["ProSubType"], callType=sn["callType"], contactCode=sn["contactCode"], BPContact=sn["BPContact"], createTime=sn["createTime"], BPPhone1=sn["BPPhone1"], BPCellular=sn["BPCellular"], BPE_Mail=sn["BPE_Mail"], BPProjCode=sn["BPProjCode"], description=sn["description"])
+        # {"priority":"priority", "subject":"subject", "problemTyp":"problemTyp", "ProSubType":"ProSubType", "callType":"callType", "contactCode":"contactCode", "BPContact":"BPContact", "createTime":"createTime", "BPPhone1":"BPPhone1", "BPCellular":"BPCellular", "BPE_Mail":"BPE_Mail", "BPProjCode":"BPProjCode", "description":"description"}
+        # sas = json.loads(ssn, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+        # sas = User(sa)
+        db.session.add(sas)
+        db.session.commit()
+        return {'Saved call': sn['subject']}
+        # return jsonify(sas)
+
+class query(Resource):
+    def get(self):
+        # stmt = db.text('''SELECT * FROM OSCL CAST('{"foo": "bar"}' as JSON) as `json`''')
+        # stmt = stmt.columns(json=db.types.JSON)
+        # u = db.engine.execute("")
+        q = db.engine.execute("SELECT * FROM OSCL")
+        # q = db.engine.execute(stmt).fetchone()
+        # return db.type(q.json)
+        s = db.select([OSCL])
+        result =  db.engine.execute(s)
+        return jsonify({'result': [dict(row) for row in q]})
+
+    def post(self):
+        sn = request.get_json()
+        # u = db.engine.execute("SELECT * FROM user")
+        # su = jsonify([dict(row) for row in u])
+
+        # se = db.text("SELECT id FROM [user] WHERE username = :ids")
+
+        se = db.text("SELECT :id FROM [user]")
+        ids = "id"
+        sele = sn["query"]
+        sele = 'id,email'
+        # u = db.engine.execute(se, sele='email', ids=ids).fetchall()
+        u = db.engine.execute(se, {'id':'idas'})
+        su = [dict(row) for row in u]
+        su = su[0]
+
+
+        # db.session.add(sas)
+        # db.session.commit()
+        return jsonify(su)
+        # return sn["query"]
+
+api.add_resource(llamadaServicio, '/llamada_servicio')
+api.add_resource(query, '/query')
 
 if __name__ == '__main__':
     app.run()
